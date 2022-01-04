7cde25fa9b7e34039d96aa7f6dd66340c44210d5
Chinmay Kulkarni <ckulkar1@tlu.ee>
2022-01-04 15:35:26 +0300

    Final submission

--- Assets/Scripts/Enemy/EnemyBehaviour.cs
+++ Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 // This script controls the behaviour of each single Alien enemy
 public class EnemyBehaviour : MonoBehaviour
 {
     public AudioSource audio;
     public AudioClip destroySFX;
+    public ScoreKeeping scoreKeeping;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       scoreKeeping = GameObject.Find("Score").GetComponent<ScoreKeeping>();
     }
 
     // Update is called once per frame
@@ -28,11 +31,20 @@ public class EnemyBehaviour : MonoBehaviour
 		//  destroy both this game object and the projectile
         if (otherCollider.tag == "Projectile")
         {
+            
             audio.PlayOneShot(destroySFX);
+            scoreKeeping.scoreNum ++;
             Destroy(gameObject);
 			
 			// Get the game object, as a whole, that's attached to the Collider2D component
             Destroy(otherCollider.gameObject);
         }
+
+         if (scoreKeeping.scoreNum == 27) 
+        {
+        SceneManager.LoadScene(0);
+        }
     }
+
+   
 }
---
+++ Assets/Scripts/UI/SceneChange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    //public AudioSource audioUI;
+    
+    
+
+    void Start()
+    {
+        //audioUI = GetComponent<AudioSource>();
+    }
+
+    public void SceneMove(int sceneId)
+    {
+        //audioUI.Play();
+        SceneManager.LoadScene(sceneId);
+        
+    }
+
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
